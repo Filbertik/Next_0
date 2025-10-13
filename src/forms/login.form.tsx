fccd6a5a6e55b5ef19a940663100f2b6fc02d64d
Filbertik <aton2019zvit@gmail.com>
2025-10-13 15:09:31 +0300

    add login form

--- src/forms/login.form.tsx
+++ src/forms/login.form.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { signInWithCredentials } from "@/actions/sign-in";
 import { Button, Form, Input } from "@heroui/react";
 import { useState } from "react";
 
@@ -7,22 +8,19 @@ interface IProps {
   onClose: () => void;
 }
 
-const RegistrationForm = ({ onClose }: IProps) => {
+const LoginForm = ({ onClose }: IProps) => {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
     confirmPassword: "",
   });
 
-  const validateEmail = (email: string) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  };
-
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    console.log("Form submitted:", formData);
-    // await registerUser(formData);
+
+    await signInWithCredentials(formData.email, formData.password);
+
+    window.location.reload();
 
     onClose();
   };
@@ -43,7 +41,6 @@ const RegistrationForm = ({ onClose }: IProps) => {
         onChange={(e) => setFormData({ ...formData, email: e.target.value })}
         validate={(value) => {
           if (!value) return "Почта обязательна";
-          if (!validateEmail(value)) return "Некорректный email";
           return null;
         }}
       />
@@ -60,26 +57,6 @@ const RegistrationForm = ({ onClose }: IProps) => {
         onChange={(e) => setFormData({ ...formData, password: e.target.value })}
         validate={(value) => {
           if (!value) return "Пароль обязателен";
-          if (value.length < 6) return "Пароль должен быть не менее 6 символов";
-          return null;
-        }}
-      />
-      <Input
-        isRequired
-        name="confirmPassword"
-        placeholder="Подтвердите пароль"
-        type="password"
-        value={formData.confirmPassword}
-        classNames={{
-          inputWrapper: "bg-default-100",
-          input: "text-sm focus:outline-none ",
-        }}
-        onChange={(e) =>
-          setFormData({ ...formData, confirmPassword: e.target.value })
-        }
-        validate={(value) => {
-          if (!value) return "Пароль для подтверждения обязателен";
-          if (value !== formData.password) return "Пароли не совпадают";
           return null;
         }}
       />
@@ -89,11 +66,11 @@ const RegistrationForm = ({ onClose }: IProps) => {
           Отмена
         </Button>
         <Button color="primary" type="submit">
-          Зарегистрироваться
+          Войти
         </Button>
       </div>
     </Form>
   );
 };
 
-export default RegistrationForm;
+export default LoginForm;
